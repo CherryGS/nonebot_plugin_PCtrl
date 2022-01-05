9a495a1ca13923299cf8e821e925ec44dc945a11
CherryGS <uplliode@gmail.com>
2022-01-05 06:03:34 +0000

    1

--- models/config.py
+++ models/config.py
@@ -3,10 +3,6 @@ from pydantic import BaseSettings
 
 class DBSettings(BaseSettings):
 
-    db_addr: str
-    db_name: str
-    db_user: str
-    db_passwd: str
     db_link: str = ""
     debug: bool
 
